f1ded06c02f79fe3bacc4bd41cd9aa5afdf006ec
Administrator <Administrator@SKY-20181109XRN>
2018-12-18 10:20:28 +0800

    123

---
+++ src/test/java/com/xys/test/MethodTest.java
@@ -0,0 +1,18 @@
+package com.xys.test;
+
+public class MethodTest {
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hello world!");
+	}
+	
+	public void test(){
+		
+		System.out.println(123);
+	}
+}
